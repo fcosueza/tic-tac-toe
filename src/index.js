090c099a61a98f7ffdc4b814c932745a1117cc49
Francisco Sueza <fcosueza@disroot.org>
2021-09-08 11:06:46 +0200

    Extract calculateWinner function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Square from "./Square";
+import calculateWinner from "./CalculateWinner";
 import "./index.css";
 
 class Board extends React.Component {
@@ -132,32 +133,6 @@ class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return {
-        winner: squares[a],
-        lines: lines[i]
-      };
-    }
-  }
-
-  return null;
 }
 
 ReactDOM.render(<Game />, document.getElementById("root"));
