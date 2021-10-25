8867f846ac1ba356752fd27508965d9a13ef6a24
Francisco Sueza <fcosueza@disroot.org>
2021-10-25 11:51:29 +0200

    A little refactor in Board component

--- src/Board.js
+++ src/Board.js
@@ -3,36 +3,33 @@ import Square from "./Square";
 import "./Board.css";
 
 const Board = props => {
-  function renderBoard(size = 3) {
-    let board = [];
+  let size = 3;
+  let board = [];
 
-    for (let i = 0; i < size; i++) {
-      let squareRow = [];
+  for (let i = 0; i < size; i++) {
+    let squareRow = [];
 
-      for (let j = 0; j < size; j++) {
-        let squareIndex = j + size * i;
+    for (let j = 0; j < size; j++) {
+      let squareIndex = j + size * i;
 
-        squareRow.push(
-          <Square
-            classes={props.winner.includes(squareIndex) ? "square highlight" : "square"}
-            value={props.squares[squareIndex]}
-            onClick={() => props.onClick(squareIndex)}
-            key={squareIndex}
-          />
-        );
-      }
-
-      board.push(
-        <div key={i} className="board-row">
-          {squareRow}
-        </div>
+      squareRow.push(
+        <Square
+          classes={props.winner.includes(squareIndex) ? "square highlight" : "square"}
+          value={props.squares[squareIndex]}
+          onClick={() => props.onClick(squareIndex)}
+          key={squareIndex}
+        />
       );
     }
 
-    return board;
+    board.push(
+      <div key={i} className="board-row">
+        {squareRow}
+      </div>
+    );
   }
 
-  return <div>{renderBoard()}</div>;
+  return <div>{board}</div>;
 };
 
 export default Board;
