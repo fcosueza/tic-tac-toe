2c7bfcadfa7fc1e411f05dcc09f83344a7b2981b
Francisco Sueza <fcosueza@disroot.org>
2021-10-26 10:24:28 +0200

    Changed variable name in calculateWinner

--- src/CalculateWinner.js
+++ src/CalculateWinner.js
@@ -1,5 +1,5 @@
 const calculateWinner = squares => {
-  const lines = [
+  const winningLines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -10,13 +10,13 @@ const calculateWinner = squares => {
     [2, 4, 6]
   ];
 
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+  for (let i = 0; i < winningLines.length; i++) {
+    const [a, b, c] = winningLines[i];
 
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return {
         winner: squares[a],
-        lines: lines[i]
+        lines: winningLines[i]
       };
     }
   }
