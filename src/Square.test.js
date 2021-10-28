851c2126b48d6c3134725a443288f4babbb282e5
Francisco Sueza <fcosueza@disroot.org>
2021-10-28 11:53:28 +0200

    Added more test to Square component

--- src/Square.test.js
+++ src/Square.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Square from "./Square";
 
 describe("Square", () => {
@@ -8,5 +9,29 @@ describe("Square", () => {
     render(<Square />);
 
     expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("Should render a square with the proper inner text", () => {
+    const value = "X";
+
+    render(<Square value={value} />);
+
+    expect(screen.getByText(value)).toBeInTheDocument();
+  });
+
+  it("Should render a square with the proper CSS classes", () => {
+    render(<Square classes="square highlight" />);
+
+    expect(screen.getByRole("button")).toHaveClass("square highlight");
+  });
+
+  it("Should call handler function when clicked", async () => {
+    const handlerMock = jest.fn();
+
+    render(<Square onClick={handlerMock} />);
+
+    userEvent.click(screen.getByRole("button"));
+
+    await waitFor(() => expect(handlerMock).toBeCalled());
   });
 });
