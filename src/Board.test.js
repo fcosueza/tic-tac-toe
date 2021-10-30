f06358f0397bca05f7ba09db27edc9f1ba0c349d
Francisco Sueza <fcosueza@disroot.org>
2021-10-30 11:08:52 +0200

    Changes variables in Board test inside it (there is only 1 test, no need to get them outside it)

--- src/Board.test.js
+++ src/Board.test.js
@@ -4,11 +4,11 @@ import { render, screen } from "@testing-library/react";
 import Board from "./Board";
 
 describe("Board", () => {
-  const size = 3;
-  const squares = Array(size * size).fill(null);
-  const winner = [1, 2, 4];
-
   it("Should render a board with the proper size", () => {
+    const size = 3;
+    const squares = Array(size * size).fill(null);
+    const winner = [1, 2, 4];
+
     render(<Board size={size} winner={winner} squares={squares} />);
 
     expect(screen.getAllByRole("button").length).toEqual(size * size);
