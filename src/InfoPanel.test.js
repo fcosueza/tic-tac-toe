b3d8c1597b959aeb23fa0320609e8f0ba2802224
Francisco Sueza <fcosueza@disroot.org>
2021-11-02 10:32:48 +0100

    Extract array from history to make testing easy

--- src/InfoPanel.test.js
+++ src/InfoPanel.test.js
@@ -3,15 +3,17 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import InfoPanel from "./InfoPanel";
 
-describe("Infopanel", () => {
-  const history = [{ squares: Array(3 * 3).fill(null), lastMove: [0, 0] }];
+describe("InfoPanel", () => {
   const status = "Next Player: X";
+  const board = Array(3 * 3).fill(null);
+  const history = [{ squares: board, lastMove: [0, 0] }];
 
   it("Should render properly the buttons and status", () => {
     const buttonText = "Go to game start";
     const sortText = "Sort Move List";
+    const stepNumber = 0;
 
-    render(<InfoPanel history={history} status={status} stepNumber={0} />);
+    render(<InfoPanel history={history} status={status} stepNumber={stepNumber} />);
 
     expect(screen.getByText(status)).toBeInTheDocument();
     expect(screen.getByText(buttonText)).toBeInTheDocument();
