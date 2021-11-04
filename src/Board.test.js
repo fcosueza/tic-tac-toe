b631edba34b35970f45ec3bd34a16d2e1db49ad5
Francisco Sueza <fcosueza@disroot.org>
2021-11-04 11:15:02 +0100

    A little refactor in Board test

--- src/Board.test.js
+++ src/Board.test.js
@@ -1,15 +1,18 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Board from "./Board";
 
 describe("Board", () => {
-  it("Should render a board with the proper size", () => {
-    const size = 3;
-    const squares = Array(size * size).fill(null);
-    const winner = [1, 2, 4];
+  const size = 3;
+  const squares = Array(size * size).fill(null);
+  const winner = [1, 2, 4];
 
+  it("Should render a board with the proper size", () => {
     render(<Board size={size} winner={winner} squares={squares} />);
 
     expect(screen.getAllByRole("button").length).toEqual(size * size);
   });
+
+  it("Should call fn when an element is clicked", async () => {});
 });
