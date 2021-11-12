a32191de54a93768b4705695c0cf01b1506ddfcc
Francisco Sueza <fcosueza@disroot.org>
2021-11-12 12:29:52 +0100

    Create setupTests.js and update tests imports

--- src/InfoPanel.test.js
+++ src/InfoPanel.test.js
@@ -1,4 +1,3 @@
-import "@testing-library/jest-dom";
 import { render, screen, within, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import InfoPanel from "./InfoPanel";
--- src/Square.test.js
+++ src/Square.test.js
@@ -1,5 +1,3 @@
-import React from "react";
-import "@testing-library/jest-dom";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Square from "./Square";
