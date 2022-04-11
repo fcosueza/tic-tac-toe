db2d3f6c6817f04b316cd780e91d7e0e1646fd08
Fco Sueza <fcosueza@disroot.org>
2022-04-11 12:15:57 +0200

    Uncatched commits (UwU)

--- src/components/InfoPanel.test.js
+++ src/components/InfoPanel.test.js
@@ -3,61 +3,61 @@ import userEvent from "@testing-library/user-event";
 import InfoPanel from "./InfoPanel";
 
 describe("InfoPanel", () => {
-  const status = "Next Player: X";
-  const board = Array(3 * 3).fill(null);
-  const history = [{ squares: board, lastMove: [0, 0] }];
-  const sortText = "Sort Move List";
-  const stepNumber = 0;
+	const status = "Next Player: X";
+	const board = Array(3 * 3).fill(null);
+	const history = [{ squares: board, lastMove: [0, 0] }];
+	const sortText = "Sort Move List";
+	const stepNumber = 0;
 
-  it("Should render properly the buttons and status", () => {
-    const buttonText = "Go to game start";
+	it("Should render properly the buttons and status", () => {
+		const buttonText = "Go to game start";
 
-    render(<InfoPanel history={history} status={status} stepNumber={stepNumber} />);
+		render(<InfoPanel history={history} status={status} stepNumber={stepNumber} />);
 
-    expect(screen.getByText(status)).toBeInTheDocument();
-    expect(screen.getByText(buttonText)).toBeInTheDocument();
-    expect(screen.getByText(sortText)).toBeInTheDocument();
-  });
+		expect(screen.getByText(status)).toBeInTheDocument();
+		expect(screen.getByText(buttonText)).toBeInTheDocument();
+		expect(screen.getByText(sortText)).toBeInTheDocument();
+	});
 
-  it("Should add a new button for every new move in history", () => {
-    const nextMove = [{ squares: board[3], lastMove: [2, 1] }];
-    const updatedHistory = history.concat(nextMove);
-    const newMoveText = "Go to move #1 (2,1)";
+	it("Should add a new button for every new move in history", () => {
+		const nextMove = [{ squares: board[3], lastMove: [2, 1] }];
+		const updatedHistory = history.concat(nextMove);
+		const newMoveText = "Go to move #1 (2,1)";
 
-    render(<InfoPanel history={updatedHistory} status={status} stepNumber={stepNumber + 1} />);
+		render(<InfoPanel history={updatedHistory} status={status} stepNumber={stepNumber + 1} />);
 
-    const moveList = screen.getAllByRole("list")[0];
+		const moveList = screen.getAllByRole("list")[0];
 
-    expect(within(moveList).getAllByRole("button").length).toBe(2);
-    expect(within(moveList).queryByText(newMoveText)).toBeInTheDocument();
-  });
+		expect(within(moveList).getAllByRole("button").length).toBe(2);
+		expect(within(moveList).queryByText(newMoveText)).toBeInTheDocument();
+	});
 
-  it("Should sort move list when sort button is clicked", async () => {
-    const nextMove = [{ squares: board[3], lastMove: [2, 1] }];
-    const updatedHistory = history.concat(nextMove);
-    const newMoveHTML = "<b>Go to move #1 (2,1)</b>";
+	it("Should sort move list when sort button is clicked", async () => {
+		const nextMove = [{ squares: board[3], lastMove: [2, 1] }];
+		const updatedHistory = history.concat(nextMove);
+		const newMoveHTML = "<b>Go to move #1 (2,1)</b>";
 
-    render(<InfoPanel history={updatedHistory} status={status} stepNumber={stepNumber + 1} />);
+		render(<InfoPanel history={updatedHistory} status={status} stepNumber={stepNumber + 1} />);
 
-    const moveList = screen.getAllByRole("list")[0];
+		const moveList = screen.getAllByRole("list")[0];
 
-    userEvent.click(screen.queryByText(sortText));
+		userEvent.click(screen.queryByText(sortText));
 
-    await waitFor(() =>
-      expect(within(moveList).queryAllByRole("button")[0].innerHTML).toBe(newMoveHTML)
-    );
-  });
+		await waitFor(() =>
+			expect(within(moveList).queryAllByRole("button")[0].innerHTML).toBe(newMoveHTML)
+		);
+	});
 
-  it("Should call fn when a button in the move list is clicked", async () => {
-    const jumpToMock = jest.fn();
-    const buttonText = "Go to game start";
+	it("Should call fn when a button in the move list is clicked", async () => {
+		const jumpToMock = jest.fn();
+		const buttonText = "Go to game start";
 
-    render(
-      <InfoPanel history={history} status={status} stepNumber={stepNumber} jumpTo={jumpToMock} />
-    );
+		render(
+			<InfoPanel history={history} status={status} stepNumber={stepNumber} jumpTo={jumpToMock} />
+		);
 
-    userEvent.click(screen.getByText(buttonText));
+		userEvent.click(screen.getByText(buttonText));
 
-    await waitFor(() => expect(jumpToMock).toBeCalledTimes(1));
-  });
+		await waitFor(() => expect(jumpToMock).toBeCalledTimes(1));
+	});
 });
