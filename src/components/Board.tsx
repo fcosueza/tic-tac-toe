022d0808a2a58e9bf25a6d80697e278ac7cff478
Fco Sueza <fcosueza@disroot.org>
2022-05-16 10:46:16 +0200

    hmmm

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,39 +3,39 @@ import Square from "./Square";
 import styles from "./Board.module.css";
 
 interface Props {
-	size: number;
-	winner: number[];
-	squares: string[];
-	onClick: (index: number) => void;
+  size: number;
+  winner: number[];
+  squares: string[];
+  onClick: (index: number) => void;
 }
 
 const Board = ({ size = 3, winner, squares, onClick }: Props) => {
-	let board: JSX.Element[] = [];
+  let board: JSX.Element[] = [];
 
-	for (let i = 0; i < size; i++) {
-		let squareRow: JSX.Element[] = [];
+  for (let i = 0; i < size; i++) {
+    let squareRow: JSX.Element[] = [];
 
-		for (let j = 0; j < size; j++) {
-			let squareIndex: number = j + size * i;
+    for (let j = 0; j < size; j++) {
+      let squareIndex: number = j + size * i;
 
-			squareRow.push(
-				<Square
-					highlight={winner.includes(squareIndex)}
-					value={squares[squareIndex]}
-					onClick={() => onClick(squareIndex)}
-					key={squareIndex}
-				/>
-			);
-		}
+      squareRow.push(
+        <Square
+          highlight={winner.includes(squareIndex)}
+          value={squares[squareIndex]}
+          onClick={() => onClick(squareIndex)}
+          key={squareIndex}
+        />
+      );
+    }
 
-		board.push(
-			<div key={i} className={styles.boardRow}>
-				{squareRow}
-			</div>
-		);
-	}
+    board.push(
+      <div key={i} className={styles.boardRow}>
+        {squareRow}
+      </div>
+    );
+  }
 
-	return <div role="grid">{board}</div>;
+  return <div role="grid">{board}</div>;
 };
 
 export default Board;
